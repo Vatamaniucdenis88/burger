af536c15e6351e820b9bf4b4f54a09c05639b513
Vatamaniucdenis88 <vatamaniuc88@gmail.com>
2020-10-04 20:22:24 +0100

    Updates

---
+++ precache-manifest.2818e2cd9c01b765d0e2405d1f622da6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b0ad2ef099089212dd6d9c1686af74",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "82d592e79edbb26f9b83",
+    "url": "/burger/static/css/main.8d15b123.chunk.css"
+  },
+  {
+    "revision": "64f637ba7951f47fdff7",
+    "url": "/burger/static/js/2.c86870bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger/static/js/2.c86870bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d592e79edbb26f9b83",
+    "url": "/burger/static/js/main.3087da69.chunk.js"
+  },
+  {
+    "revision": "7353b53bfa636296db64",
+    "url": "/burger/static/js/runtime-main.8b4707a1.js"
+  },
+  {
+    "revision": "cb66f89eab2a7bffa71321088d9a140e",
+    "url": "/burger/static/media/1.cb66f89e.jpg"
+  },
+  {
+    "revision": "0432b2a44d28bd988c174fbea3fdada1",
+    "url": "/burger/static/media/chips.0432b2a4.png"
+  },
+  {
+    "revision": "15a62c680a4f28d3f3ffdde978333e8d",
+    "url": "/burger/static/media/cup.15a62c68.png"
+  },
+  {
+    "revision": "c315d8e04dfabce0f1abbee016a27401",
+    "url": "/burger/static/media/navlogo.c315d8e0.png"
+  },
+  {
+    "revision": "70d8f7896ec5a4df4337d87c17f2a488",
+    "url": "/burger/static/media/salat.70d8f789.png"
+  },
+  {
+    "revision": "1915b5f9f126308c1c359f3598578b9a",
+    "url": "/burger/static/media/top.1915b5f9.png"
+  }
+]);
